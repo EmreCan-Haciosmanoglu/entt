17392111d21ca2ba62c76dec8c5284268b97b7e4
Emre Can Hacıosmanoğlu <emrecan.haciosmanoglu@agu.edu.tr>
2020-07-16 22:42:24 +0300

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "EnTT"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+    
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "single_include/entt/entt.hpp",
+    }
+    
+    includedirs
+    {
+        "include"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
+        
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+    
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
